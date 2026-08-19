939a5cc17a728a532dc68ecbeb02fc320ffdc550
Hugo Viana <hv.pvdd@gmail.com>
2026-08-18 23:00:56 -0300

    Commit Inicial

---
+++ src/test/java/com/hvsoftware/almox_sb/AlmoxsbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvsoftware.almox_sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlmoxsbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
